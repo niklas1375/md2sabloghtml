2e0ea6872c139633204dc3ac61fb32229addcc5c
Niklas <niklas1375@gmail.com>
2023-03-22 11:15:15 +0100

    put output file in input file's dir

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { readFileSync, writeFileSync, existsSync } from 'fs';
 import { marked } from 'marked';
 import { renderer } from './helper.js';
+import * as path from 'path';
 
 const DEFAULT_INPUT_FILENAME = 'input.md';
 const DEFAULT_OUTPUT_FILENAME = 'blog.html';
@@ -8,13 +9,18 @@ const DEFAULT_OUTPUT_FILENAME = 'blog.html';
 let inputFilename;
 let outputFilename;
 if (process.argv.length < 3 || process.argv[2]?.length <= 0) {
+    // no additional parameters provided
     inputFilename = DEFAULT_INPUT_FILENAME;
 } else {
+    // input file name provided
     inputFilename = process.argv[2]
 }
 if (process.argv.length < 4 || process.argv[3]?.length <= 0) {
-    outputFilename = DEFAULT_OUTPUT_FILENAME;
+    // no output file name provided
+    const parentDir = path.dirname(inputFilename);
+    outputFilename = parentDir + "/" + DEFAULT_OUTPUT_FILENAME;
 } else {
+    // output file name provided
     outputFilename = process.argv[3]
 }
 if (!existsSync(inputFilename)) {
